bf70a9296b4e4a3765b420444bac7e483dc900c2
tuxkids <ukie.tux@gmail.com>
2014-01-15 15:53:41 +0700

    add battery calibration

--- src/com/tuxkids/batterynotification/Advance.java
+++ src/com/tuxkids/batterynotification/Advance.java
@@ -1,50 +1,49 @@
 package com.tuxkids.batterynotification;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
+import android.R.string;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.widget.Toast;
 
 public class Advance extends PreferenceActivity {
 	
+	
 	CheckBoxPreference cb1;
-	boolean fast;
+	boolean test = false;
 	static CMDProcessor cmd = new CMDProcessor();
 	private final static String fastcharge = "/sys/kernel/fast_charge/force_fast_charge";
+	final String close = "exit";
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
- 
         addPreferencesFromResource(R.xml.advance_screen);
-        //set checked state
+        
+        //set checked state #fastcharge
         setChecked();
+        checkFastSupport();    
         
-        //set value fast charge
-        setValue();
-        
-        
+        //battery calibration
+        calibration();
     }
 	
+	//FastCharge
 	public void setValue(){
 		//exec command
 		final String enable  = "echo 1 > "+fastcharge;
-		final String disable = "echo 0 > "+fastcharge;
-		final String close = "exit";	
+		final String disable = "echo 0 > "+fastcharge;	
         
 		//get on change checkbox preference
-        super.findPreference("preffast").
+        findPreference("preffast").
         setOnPreferenceChangeListener( new OnPreferenceChangeListener(){
 			@Override
 			public boolean onPreferenceChange(Preference preference,
@@ -65,32 +64,19 @@ public class Advance extends PreferenceActivity {
         	
 	}
 	
-	public void getChecked (){
-		SharedPreferences sharedPrefs = PreferenceManager
-        .getDefaultSharedPreferences(this);		
-		fast = sharedPrefs.getBoolean("preffast", false);		
-	}
+	
 
 	public void setChecked(){
-		getChecked();
-		cb1 = (CheckBoxPreference)findPreference("preffast");
+		cb1 = (CheckBoxPreference) findPreference("preffast");
 		String status = getCurrentStatus();
 		String aktif = "1";
 		String tdk="0";
 		
-		//check status by value on status
+		//set checked by value fast charge
 		if ((status.equals(aktif)) ){
-			fast=true;
-		}
-		else if ((status.equals(tdk)) ){
-			fast=false;
-		}
-		
-		//after get status then execute by fast value
-		if (fast==true){
 			cb1.setChecked(true);
 		}
-		else if (fast==false) {
+		else if ((status.equals(tdk)) ){
 			cb1.setChecked(false);
 		}
 	}
@@ -98,12 +84,13 @@ public class Advance extends PreferenceActivity {
 	
 	public void checkFastSupport(){
 		File myFile = new File(fastcharge);
-		
 		if (myFile.exists()) {
+			//set value fast charge
+	        setValue();
 			}
 		else {
-				alert();
 				cb1.setEnabled(false);
+				cb1.setSummary("You're kernel not support fast charge");
 			}
 		}
 	
@@ -111,21 +98,48 @@ public class Advance extends PreferenceActivity {
 		return cmd.readString(fastcharge);
 	}
 
-	
-	public void alert (){
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setCancelable(false)
-		       .setTitle("Alert")
-		       .setMessage(
-		    		"Your Kernel Not Support Fast Charge\n" +
-		    		"Make sure you're kernel have fast charge future"+
-		    		"to use this option")
-		       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {
-		                dialog.cancel();
-		           }
-		       });
-		AlertDialog alert = builder.create();
-		alert.show();
-		}
+	//Battery Calibration
+	public void calibration(){
+		Preference kalibrasi = findPreference("kalibrasi");
+		kalibrasi.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				CalibrationAlert();
+				return false;
+			}
+		});
 	}
+	
+	public void CalibrationAlert(){
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setCancelable(true)
+			       .setTitle("Alert")
+			       .setMessage(
+			    		"Are you sure to calibration your battery ?")
+			       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			               //do calibration 
+			        	   String kalibrasi = "rm /data/system/batterystats.bin";
+			                cmd.execCommand(kalibrasi);
+			                cmd.execCommand(close);
+			                
+			              //show toast notification
+			              //show message error when kernel not support
+							Context context = getApplicationContext();
+							CharSequence text1 = "/data/system/batterystats.bin was removed";
+							int duration = Toast.LENGTH_SHORT;
+							Toast toast = Toast.makeText(context, text1, duration);
+							toast.show();
+			                dialog.cancel();
+			           }
+			       })
+			       .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			                dialog.cancel();
+			           }
+			       });
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
+}
